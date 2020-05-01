f8faab019f2b5c6f1525d48316519f0fec490b6d
SelVic <limelefthand@gmail.com>
2020-05-01 23:06:59 +0300

    upd

---
+++ components/MapPage.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+
+class MapsPage extends Component {
+    componentDidMount() {
+        let foo = () => {
+            this.myMap = new ymaps.Map("map", {center: [55.76, 37.64], zoom: 7})
+        };
+        window.ymaps.ready(foo);
+    }
+    componentWillUnmount() {
+        this.myMap.destroy();
+    }
+
+    myMap = null;
+
+    render() {
+        return (
+            <div className="mapSize" id="map"></div>
+        )
+    }
+}
+export {MapsPage}
