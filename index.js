21f8c1d1369617f4ec6fa060ae1249ba0a184b97
SelVic <limelefthand@gmail.com>
2020-07-27 14:22:59 +0300

    node_modules removed

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ import {Cycle1} from "./scripts/timer.js";
 import {Extract} from "./scripts/extract.js";
 import {MapPage} from "./pages/MapPage.jsx";
 
-//доделать цсс в картах
-//поправить зум
-//@media
-//
+
 class App extends Component {
     render() {
         return (
