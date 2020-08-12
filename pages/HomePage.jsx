6e62ac2e1bd3302aeaeeec6373311d3a4d110ec1
SelVic <limelefthand@gmail.com>
2020-08-12 16:23:28 +0300

    upd

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -5,7 +5,7 @@ function Home(){
         <div className="jumbo flex-row-center">
             <div className="jumbo-Container">
                 <h1 className="jumbo-title">Home page</h1>
-                <p className="jumbo-text">Баловство с CSS</p>
+                <p className="jumbo-text">CSS</p>
             </div>
         </div>
     )
